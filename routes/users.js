82679c83cc359b5d39a68f88a311a5c28150919b
Mamadou <sallmahdou1@gmail.com>
2024-07-02 22:06:22 +0200

    feat: add user token for nfc

--- routes/users.js
+++ routes/users.js
@@ -25,7 +25,35 @@ router.post("/user/auth", async (req, res) => {
 
         return res.status(200).json({ message: "SUCCESS", data : {sessionToken }});
       } catch (error) {
-        return res.status(401).json({ message: 'Invalid token' });
+        return res.status(500).json({ message: error.message });
+      }
+})
+
+router.post("/nfc/generate-token", async (req, res) => {
+    const { email, role, name } = req.body;
+
+    if (!email | !name | !role) {
+        return res.status(400).json({ message: 'Required attribut is missing : email | role | name' });
+      }
+    
+    try {
+        const findUser = await User.findOne({ email });
+        if (findUser) {
+          return res.status(400).json({ message: "Email user already exist ! " });
+        }
+        const userInfo = {
+            name,
+            email,
+            role,
+          }
+        const nfcToken = jwt.sign(userInfo, sessionSecret, { expiresIn: '8h' });
+        const newUser = new User(userInfo)
+        const saveUser = await newUser.save();
+        
+        return res.status(200).json({ message: "SUCCESS", data : { nfcToken, user : saveUser }});
+      } catch (error) {
+        console.error("Error : ", error);
+        return res.status(500).json({ message: error.message });
       }
 })
 
